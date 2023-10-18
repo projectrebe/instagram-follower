f0fe7715d8622177859aa4bd822f6f37071ecea3
parwez-zafar <parwezzafar3@gmail.com>
2023-10-18 11:24:33 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <center>
+        Count Your Instagram followers
+      </center>
+    </div>
+  );
+}
+
+export default App;

99e0d7ac30307478fe578ce06340a1fc912f4235
parwez-zafar <parwezzafar3@gmail.com>
2023-10-20 11:36:49 +0530

    making dynamic

--- src/component/InstagramFollower/InstagramFollower.js
+++ src/component/InstagramFollower/InstagramFollower.js
@@ -1,43 +1,57 @@
 import React, { useState } from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { saveUser } from '../../action/userAction'
 // import User from '../User/User'
-// import axios from 'axios'
+import axios from 'axios'
 const InstagramFollower = () => {
     const [name, setName] = useState('')
     const [username, setUsername] = useState('')
     const [count, setCount] = useState()
     const dispatch = useDispatch();
-    // const { error, loading, isAuthenticated } = useSelector((state) => state.user)
+    const { error, loading, isAuthenticated } = useSelector((state) => state.user)
     // let;
     const getCount = async (e) => {
         e.preventDefault();
-        // console.log("sdf");
 
-        // const options = {
-        //     method: 'GET',
-        //     url: `https://instagram-profile1.p.rapidapi.com/getprofileinfo/${name}`,
-        //     headers: {
-        //         'X-RapidAPI-Key': '805d2fcbd4msh43077584510c2aep1360cajsnfe2cd36cb03a',
-        //         'X-RapidAPI-Host': 'instagram-profile1.p.rapidapi.com'
-        //     }
-        // };
+        console.log("sdf");
 
-        // try {
-        //     console.log('entered');
-        //     const response = await axios.request(options);
-        //     console.log(response.data);
-        //     setCount(response.data.followers);
-        // } catch (error) {
-        //     console.error(error);
-        // }
 
-        const userData = {
-            "name": name,
-            "username": username
+
+        const options = {
+            method: 'POST',
+            url: 'https://instagram120.p.rapidapi.com/api/instagram/profile',
+            headers: {
+                'content-type': 'application/json',
+                'X-RapidAPI-Key': '805d2fcbd4msh43077584510c2aep1360cajsnfe2cd36cb03a',
+                'X-RapidAPI-Host': 'instagram120.p.rapidapi.com'
+            },
+            data: {
+                username: `${username}`
+            }
+        };
+
+        try {
+            const response = await axios.request(options);
+            console.log(response.data.result);
+
+            console.log(response.data);
+            setCount(response.data.result.edge_followed_by.count);
+            const userData = {
+                "name": name,
+                "username": username,
+                "count": response.data.result.edge_followed_by.count
+            }
+            dispatch(saveUser(userData))
+        } catch (error) {
+            console.error(error);
         }
-        dispatch(saveUser(userData))
-        setCount(10);
+
+
+
+        // setCount(15)
+        // console.log(count);
+
+        // setCount(10);
 
     }
 
@@ -71,7 +85,7 @@ const InstagramFollower = () => {
 
 
 
-                    <button type="submit" className="btn btn-primary">Submit</button>
+                    <button type="submit" className="btn btn-primary">{loading ? "loading..." : "Sumbit"}</button>
                 </form>
             </div >
             {/* <User /> */}
